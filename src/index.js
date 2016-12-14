71572bc286fccbc91f40419b0e252bee969f2b52
Tri Nguyen <mail@trimn.net>
2016-12-14 23:32:16 +0100

    Update algorithm - haversine is slightly faster than the spherical cosine

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ const to_rad = (num) => num * Math.PI / 180
 const haversine = (point1, point2) => {
   const φ1 = to_rad(point1.latitude)
   , φ2 = to_rad(point2.latitude)
+  , Δφ = to_rad(point2.latitude - point1.latitude)
   , Δλ = to_rad(point2.longitude - point1.longitude)
 
-  const distance = Math.acos(Math.sin(φ1) * Math.sin(φ2) + Math.cos(φ1) * Math.cos(φ2) * Math.cos(Δλ)) * EARTH_RADIUS_METER
+  const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) + Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ/2) * Math.sin(Δλ/2)
+  , c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+  , distance = EARTH_RADIUS_METER * c
 
   return distance
 }
